abc60e17665de5c584eff8fdde8941c834fda54c
朱代雷 <zdl328465042@163.com>
2020-12-23 17:41:32 +0800

    修改ZVisibilityExtend错误

--- lib/ui/extend/z_visibility_extend.dart
+++ lib/ui/extend/z_visibility_extend.dart
@@ -16,7 +16,7 @@ class ZVisibilityExtend extends StatelessWidget {
     return Visibility(
       visible: visible != Visible.GONE,
       child: Opacity(
-        opacity: visible == Visible.INVISIBLE ? 1.0 : 0.0,
+        opacity: visible == Visible.INVISIBLE ? 0.0 : 1.0,
         child: child,
       ),
     );
