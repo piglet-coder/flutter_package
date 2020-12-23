a7a804c1ddf83f95676d5884c91c109dbe810843
朱代雷 <zdl328465042@163.com>
2020-12-23 15:03:05 +0800

    新增ZChineseCupertinoLocalizations

---
+++ lib/extend/z_chinese_cupertino_localizations.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+/// @author zdl
+/// date 2020/12/23 15:02
+/// email zdl328465042@163.com
+/// description 国际化，ios支持中文
+/*
+
+localizationsDelegates: <LocalizationsDelegate<dynamic>>[
+        ChineseCupertinoLocalizations.delegate, // 自定义的delegate
+
+        DefaultCupertinoLocalizations.delegate, // 目前只包含英文
+
+        // 下面两个是Material widgets的delegate, 包含中文
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+
+*/
+class ZChineseCupertinoLocalizations implements CupertinoLocalizations {
+  final materialDelegate = GlobalMaterialLocalizations.delegate;
+  final widgetsDelegate = GlobalWidgetsLocalizations.delegate;
+  final local = const Locale('zh');
+
+  MaterialLocalizations ml;
+
+  Future init() async {
+    ml = await materialDelegate.load(local);
+    print(ml.pasteButtonLabel);
+  }
+
+
+  @override
+  String get alertDialogLabel => ml.alertDialogLabel;
+
+  @override
+  String get anteMeridiemAbbreviation => ml.anteMeridiemAbbreviation;
+
+  @override
+  String get copyButtonLabel => ml.copyButtonLabel;
+
+  @override
+  String get cutButtonLabel => ml.cutButtonLabel;
+
+  @override
+  DatePickerDateOrder get datePickerDateOrder => DatePickerDateOrder.mdy;
+
+  @override
+  DatePickerDateTimeOrder get datePickerDateTimeOrder =>
+      DatePickerDateTimeOrder.date_time_dayPeriod;
+
+  @override
+  String datePickerDayOfMonth(int dayIndex) {
+    return dayIndex.toString();
+  }
+
+  @override
+  String datePickerHour(int hour) {
+    return hour.toString().padLeft(2, "0");
+  }
+
+  @override
+  String datePickerHourSemanticsLabel(int hour) {
+    return "$hour" + "时";
+  }
+
+  @override
+  String datePickerMediumDate(DateTime date) {
+    return ml.formatMediumDate(date);
+  }
+
+  @override
+  String datePickerMinute(int minute) {
+    return minute.toString().padLeft(2, '0');
+  }
+
+  @override
+  String datePickerMinuteSemanticsLabel(int minute) {
+    return "$minute" + "分";
+  }
+
+  @override
+  String datePickerMonth(int monthIndex) {
+    return "$monthIndex";
+  }
+
+  @override
+  String datePickerYear(int yearIndex) {
+    return yearIndex.toString();
+  }
+
+  @override
+  String get pasteButtonLabel => ml.pasteButtonLabel;
+
+  @override
+  String get postMeridiemAbbreviation => ml.postMeridiemAbbreviation;
+
+  @override
+  String get selectAllButtonLabel => ml.selectAllButtonLabel;
+
+  @override
+  String get modalBarrierDismissLabel => ml.modalBarrierDismissLabel;
+
+  @override
+  String get todayLabel => '今天';
+
+  @override
+  String tabSemanticsLabel({int tabIndex, int tabCount}) {
+    // TODO: implement tabSemanticsLabel
+    throw UnimplementedError();
+  }
+
+  @override
+  String timerPickerHour(int hour) {
+    return hour.toString().padLeft(2, "0");
+  }
+
+  @override
+  String timerPickerHourLabel(int hour) {
+    return "$hour".toString().padLeft(2, "0") + "时";
+  }
+
+  @override
+  String timerPickerMinute(int minute) {
+    return minute.toString().padLeft(2, "0");
+  }
+
+  @override
+  String timerPickerMinuteLabel(int minute) {
+    return minute.toString().padLeft(2, "0") + "分";
+  }
+
+  @override
+  String timerPickerSecond(int second) {
+    return second.toString().padLeft(2, "0");
+  }
+
+  @override
+  String timerPickerSecondLabel(int second) {
+    return second.toString().padLeft(2, "0") + "秒";
+  }
+
+  static const LocalizationsDelegate<CupertinoLocalizations> delegate =
+  _ChineseDelegate();
+
+  static Future<CupertinoLocalizations> load(Locale locale) async {
+    var localizaltions = ZChineseCupertinoLocalizations();
+    await localizaltions.init();
+    return SynchronousFuture<CupertinoLocalizations>(localizaltions);
+  }
+}
+
+class _ChineseDelegate extends LocalizationsDelegate<CupertinoLocalizations> {
+  const _ChineseDelegate();
+
+  @override
+  bool isSupported(Locale locale) {
+    return locale.languageCode == 'zh';
+  }
+
+  @override
+  Future<CupertinoLocalizations> load(Locale locale) {
+    return ZChineseCupertinoLocalizations.load(locale);
+  }
+
+  @override
+  bool shouldReload(LocalizationsDelegate<CupertinoLocalizations> old) {
+    return false;
+  }
+}
