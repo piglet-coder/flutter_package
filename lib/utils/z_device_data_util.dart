4fa3efdc0ca4e2b518e2ed2294b3726bee977b78
朱代雷 <zdl328465042@163.com>
2020-12-23 18:48:19 +0800

    修改ZDeviceDataUtil

--- lib/utils/z_device_data_util.dart
+++ lib/utils/z_device_data_util.dart
@@ -13,6 +13,9 @@ class ZDeviceDataUtil{
 
   static MediaQueryData _mediaQueryData = MediaQueryData.fromWindow(window);
 
+  /// padding
+  static EdgeInsets get padding => _mediaQueryData.padding;
+
   ///屏幕宽
   static double get screenWidth => _mediaQueryData.size.width;
 
@@ -20,10 +23,10 @@ class ZDeviceDataUtil{
   static double get screenHeight => _mediaQueryData.size.height;
 
   ///获取上方状态栏高度
-  static double topBarHeight = _mediaQueryData.padding.top;
+  static double topBarHeight = padding.top;
 
   ///获取下方状态栏高度
-  static double botBarHeight = _mediaQueryData.padding.bottom;
+  static double botBarHeight = padding.bottom;
 
   ///当前屏幕软键盘高度，0代表软键盘未弹出，前提是所有父布局resizeToAvoidBottomInset=false
   static double keyboardHeight(BuildContext context) => MediaQuery.of(context).viewInsets.bottom;
