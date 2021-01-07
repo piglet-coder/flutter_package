a3e9cdd4c06469954a404ab0f60d46e6916e861e
朱代雷 <zdl328465042@163.com>
2021-01-07 15:26:11 +0800

    新增扩展方法，用于获取枚举name

--- lib/extension/extension_list.dart
+++ lib/extension/extension_list.dart
@@ -4,14 +4,25 @@ import 'extension_string.dart';
 /// date 2020/10/22 14:50
 /// email zdl328465042@163.com
 /// description 扩展List
-extension ExtensionList on List{
-
+extension ExtensionList on List {
   bool get isList => null != this || !(this is List);
 
   List get dealNotList => isList ? this : [];
 
-  String toStringWith({String splitUnit = '、', String def = '无'}){
+  /// 拆分列表后拼接为字符串
+  String toStringWith({String splitUnit = '、', String def = '无'}) {
     String str = this.dealNotList.toString();
-    return str.substring(1, str.length-1).replaceAll(', ', splitUnit).dealData(def: def);
+    return str
+        .substring(1, str.length - 1)
+        .replaceAll(', ', splitUnit)
+        .dealData(def: def);
+  }
+
+  /// 获取枚举中自定义的值
+  String getNameAt(int index) {
+    if (this.dealNotList.length <= index)
+      throw ('ArrayIndexOutOfBoundsException');
+    else
+      return this[index].toString().split('.').last;
   }
 }
