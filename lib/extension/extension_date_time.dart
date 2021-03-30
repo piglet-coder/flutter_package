9f69ae056e1849a4825b2023cc14e1fff7d308ee
朱代雷 <zdl328465042@163.com>
2021-03-30 10:42:28 +0800

    添加ExtensionDateTime

---
+++ lib/extension/extension_date_time.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_package/flutter_package.dart';
+import 'package:intl/intl.dart';
+
+/// @author zdl
+/// date 2021/03/29 17:19
+/// email zdl328465042@163.com
+/// description DateTime扩展类
+extension ExtensionDateTime on DateTime{
+
+  /// yyyy-MM-dd HH:mm:ss
+  String get toDef => DateFormat(ZTimeFormat.def).format(this);
+
+  /// yyyy年MM月dd日 HH点mm分ss秒
+  String get toDefZh => DateFormat(ZTimeFormat.def_zh).format(this);
+
+  /// yyyy-MM-dd HH:mm
+  String get toYmdHm => DateFormat(ZTimeFormat.ymd_hm).format(this);
+
+  /// yyyy年MM月dd日 HH点mm分
+  String get toYmdHmZh => DateFormat(ZTimeFormat.ymd_hm_zh).format(this);
+
+  /// yyyy-MM-dd
+  String get toYmd => DateFormat(ZTimeFormat.ymd).format(this);
+
+  /// yyyy年MM月dd日
+  String get toYmdZh => DateFormat(ZTimeFormat.ymd_zh).format(this);
+
+  /// MM-dd
+  String get toMd => DateFormat(ZTimeFormat.md).format(this);
+
+  /// MM月dd日
+  String get toMdZh => DateFormat(ZTimeFormat.md_zh).format(this);
+
+  /// HH:mm:ss
+  String get toHms => DateFormat(ZTimeFormat.hms).format(this);
+
+  /// HH点mm分ss秒
+  String get toHmsZh => DateFormat(ZTimeFormat.hms_zh).format(this);
+
+  /// yyyy-MM-dd\'T\'HH:mm:ss\'.0Z\'
+  String get toRfc3339 => DateFormat(ZTimeFormat.rfc3339).format(this);
+
+  /// DateTime转字符串
+  String toStr(String format){
+    assert(format != null, 'format不可为null');
+    try{
+      return DateFormat(format).format(this);
+    }catch(_){
+      throw('format格式不符合规范，当前format为($format)');
+    }
+  }
+}
