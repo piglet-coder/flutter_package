b3c0b64e372e4a413cc30470bade06985eb85bd9
朱代雷 <zdl328465042@163.com>
2021-05-26 14:48:24 +0800

    颜色添加#

--- lib/extension/extension_color.dart
+++ lib/extension/extension_color.dart
@@ -10,6 +10,6 @@ import 'extension_num.dart';
 extension ExtensionColor on Color {
 
   /// 解析颜色为字符串，忽略透明度
-  String get getColorStr => '${this.red.toHex}${this.blue.toHex}${this.green.toHex}';
-  // String get getColorStr => '${this.alpha.toHex}${this.red.toHex}${this.blue.toHex}${this.green.toHex}';
+  String get getColorStr => '#${this.red.toHex}${this.blue.toHex}${this.green.toHex}';
+  // String get getColorStr => '#${this.alpha.toHex}${this.red.toHex}${this.blue.toHex}${this.green.toHex}';
 }
