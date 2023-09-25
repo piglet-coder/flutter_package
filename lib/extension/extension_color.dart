0a5f675a0195e0074b3c0c39fe79362af050489b
zdl <zdl328465042@163.com>
2023-09-25 14:51:01 +0800

    解析颜色为字符串，忽略透明度

--- lib/extension/extension_color.dart
+++ lib/extension/extension_color.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'extension_string.dart';
-import 'extension_num.dart';
-
 /// @author zdl
 /// date 2021/05/26 14:24
 /// email zdl328465042@163.com
@@ -10,6 +7,5 @@ import 'extension_num.dart';
 extension ExtensionColor on Color {
 
   /// 解析颜色为字符串，忽略透明度
-  String get getColorStr => '#${this.red.toHex}${this.blue.toHex}${this.green.toHex}';
-  // String get getColorStr => '#${this.alpha.toHex}${this.red.toHex}${this.blue.toHex}${this.green.toHex}';
+  String get getColorStr => '#${this.value.toRadixString(16)}';
 }
